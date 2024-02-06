752ec26072840f3a2a16c18c40bd27b8095c3a62
priyanshu09143 <vishal9772345@gmail.com>
2024-02-06 08:51:00 +0530

    env Change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(UserRoutes)
 app.use(TaskRoutes)
 app.use(cors({
-    origin:[process.env.FRONTEND],
+    origin:[process.env.FRONTEND_URI],
     methods: [ "GET", "POST", "PUT","DELETE"],
     credentials:true,
 }))
